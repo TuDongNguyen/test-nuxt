4f4539b1644d9f45b285314ec7dfa5b81de3dcfe
TuDongNguyen <tu_dong@allgrow-labo.jp>
2024-11-07 16:13:22 +0700

    no message

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,6 +1,17 @@
 import type { RouterConfig } from "nuxt/schema";
 export default {
   scrollBehavior: () => {
+    console.log("scroll");
+    function test() {
+      console.log("test");
+    }
+    test();
+    const element = document.createElement("span");
+    element.innerHTML = "test";
+    document.body.append(element);
+
+    alert("alert");
+
     return {
       top: 50,
       left: 0,
